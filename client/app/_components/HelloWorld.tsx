617d34b5b2da67d5251c9cc7562b0a8d100ba7db
namay26 <namayrohatgi@gmail.com>
2025-02-01 04:23:21 +0530

    Initial Commit

---
+++ client/app/_components/HelloWorld.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+
+export default function HelloWorld() {
+
+  return (
+    <div className="flex flex-col items-center w-full space-y-6 p-4">
+      HELLO WORLD
+    </div>
+  );
+}
